555ca293f6eca5ba26a4c8270cf617cdfef5a010
Dethie DIOUF <55641058+DETHIEDIOUF4@users.noreply.github.com>
2022-08-17 13:49:35 +0000

    Initial Commit

---
+++ PortFolio/PortFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortFolioApp.swift
+//  PortFolio
+//
+//  Created by Dethie on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PortFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
